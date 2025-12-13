67151ded23024c171262ad7c48aad4559fe6a9b4
Phi-C <chenxjhit@163.com>
2025-12-13 22:37:12 +0800

    update

---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -ex
+
+# Function to check if a package is installed
+is_installed() {
+    command -v "$1" &>/dev/null
+}
+
+# Install package if not already installed
+install_package() {
+    local package_name="$1"
+    local install_cmd="$2"
+
+    if is_installed "$package_name"; then
+        echo "$package_name is already installed. Skipping..."
+    else
+        echo "Installing $package_name..."
+        eval "$install_cmd"
+    fi
+}
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS
+    echo "Detected macOS"
+    INSTALLER="brew install"
+    INSTALLER_FD="brew install fd"
+elif [[ -f "/etc/os-release"]] && grep -qi "ubuntu" /etc/os-release; then
+    # Ubuntu
+    echo "Detected Ubuntu"
+    INSTALLER="sudo apt install -y"
+    INSTALLER_FD="sudo apt install -y fd-find && ln -s $(which fdfind) ~/.local/bin/fd"
+elif [[ -f "/etc/os-release"]] && grep -qi "centos" /etc/os-release; then
+    # CentOS
+    echo "Detected CentOS"
+else
+    echo "Unsupported OS."
+    exit 1
+fi
+
+
+# Install necessary packages
+install_package "rg" "$INSTALLER ripgrep"
+install_package "fd" "$INSTALLER_FD"
+
+# Install zsh from source
+if [[ -f ~/.zshrc ]]; then
+    echo "~/.zshrc already exists. Backing up..."
+    mv ~/.zshrc ~/.zshrc.backup.$(date +%Y.%m.%d.%H%M%S)
+fi
+bash zsh/install_zsh.sh
+
+
+# Install tmux from source
+if [[ -f ~/.tmux.conf ]]; then
+    echo "~/.tmux.conf already exists. Backing up..."
+    mv ~/.tmux.conf ~/.tmux.conf.backup.$(date +%Y.%m.%d.%H%M%S)
+fi
+bash tmux/install_tmux.sh
+
+# process tmux config
+cp tmux/.tmux.conf ~/.tmux.conf
+mkdir -p "~/.config"
+
+cp -a 3rdparty ~/.config
--- vim/README.md
+++ vim/README.md
@@ -19,3 +19,11 @@ https://github.com/ruanyl/vim-gh-line
 使用方法:
 1. \<leader>gh: blob view
 2. \<leader>gb: blame view
+
+### 3.2 参考
+- [junegunn/vim-plug](https://github.com/junegunn/vim-plug) - :hibiscus: Minimalist Vim Plugin Manager
+- [preservim/nerdtree](https://github.com/preservim/nerdtree) - A tree explorer plugin for vim.
+- [spf13/spf13-vim](https://github.com/spf13/spf13-vim) - The ultimate vim distribution
+- [TTWShell/legolas-vim](https://github.com/TTWShell/legolas-vim) - Vim配置，为python、go开发者打造的IDE。
+- [amix/vimrc](https://github.com/amix/vimrc) - The ultimate Vim configuration (vimrc)
+- [How to Do 90% of What Plugins Do (With Just Vim)](https://www.youtube.com/watch?v=XA2WjJbmmoM) -  recommend
